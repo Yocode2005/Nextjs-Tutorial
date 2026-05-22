af956b248c23990d7345d57f08685f81252ea43b
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-22 10:37:27 +0530

    add comments for better understanding

--- hello-world/src/app/products/[productid]/reviews/[reviewId]/not-found.tsx
+++ hello-world/src/app/products/[productid]/reviews/[reviewId]/not-found.tsx
@@ -2,12 +2,12 @@
 import { usePathname } from "next/navigation";
 export default function NotFound(){
     const pathname = usePathname();
-    const productid = pathname.split("/")[2];
-    const reviewId = pathname.split("/")[4];
+    const productId = pathname.split("/")[2]; // Extract productId from the URL and 2
+    const reviewId = pathname.split("/")[4]; // Extract reviewId from the URL and 4 is the index of reviewId in the URL
     return (
         <div>
             <h2>Review Not Found</h2>
-            <p>Could not find requested review {reviewId} for product {productid}</p>
+            <p>Could not find requested review {reviewId} for product {productId}</p>
         </div>
     );
 }
