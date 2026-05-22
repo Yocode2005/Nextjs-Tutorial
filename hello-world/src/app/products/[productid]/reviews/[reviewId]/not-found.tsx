afdf8e837d57b878589650e9b39f9e2558eaac94
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-22 10:30:35 +0530

    explore usePathname hook

--- hello-world/src/app/products/[productid]/reviews/[reviewId]/not-found.tsx
+++ hello-world/src/app/products/[productid]/reviews/[reviewId]/not-found.tsx
@@ -1,8 +1,13 @@
+"use client";
+import { usePathname } from "next/navigation";
 export default function NotFound(){
+    const pathname = usePathname();
+    const productid = pathname.split("/")[2];
+    const reviewId = pathname.split("/")[4];
     return (
         <div>
             <h2>Review Not Found</h2>
-            <p>Could not find requested review from the resources</p>
+            <p>Could not find requested review {reviewId} for product {productid}</p>
         </div>
     );
 }
