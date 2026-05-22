a31da363e28a1b2c33d01f110b26fc7e422d14a6
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-22 22:38:18 +0530

    dynamic metadata routing

--- hello-world/src/app/products/[productid]/page.tsx
+++ hello-world/src/app/products/[productid]/page.tsx
@@ -1,8 +1,21 @@
-export default async function ProductDetails({
-    params,
-} : {
-    params : Promise<{ productid : string }>;
-}){
-    const productid = (await params).productid;
-    return <h1>Details about product {productid}</h1>;
+import { Metadata } from "next";
+type Props = {
+  params: Promise<{ productid: string }>;
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const id = (await params).productid;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${id}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
+  const productid = (await params).productid;
+  return <h1>Details about product {productid}</h1>;
 }
