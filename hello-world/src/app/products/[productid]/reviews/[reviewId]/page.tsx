0128cda3ed3235321917917d819f5e32dee0690f
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-22 10:20:56 +0530

    add especific notFound page

--- hello-world/src/app/products/[productid]/reviews/[reviewId]/page.tsx
+++ hello-world/src/app/products/[productid]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from 'next/navigation';
+import { parse } from 'path';
 export default async function ProductReview({
     params,
 } : {
     params : Promise<{ productid : string;  reviewId : string }>;
 }){
     const { productid, reviewId } = await params;
+    if(parseInt(reviewId) > 100){
+        notFound();
+    }
     return <h1>Review {reviewId} for product {productid}</h1>;  
 }
