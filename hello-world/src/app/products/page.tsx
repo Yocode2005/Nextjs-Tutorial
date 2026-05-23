3087e1d16e0f086fb54bf38d7d9d569182273b11
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-23 22:12:31 +0530

    add commets for better understanding of the code

--- hello-world/src/app/products/page.tsx
+++ hello-world/src/app/products/page.tsx
@@ -18,7 +18,7 @@ export default function ProductList() {
         <Link href="/products/4">Product 4</Link>
       </h2>
       <h2>
-        <Link href="/products/5" replace>Product 5</Link>
+        <Link href="/products/5" replace>Product 5</Link> // replace will replace the current entry in the history stack instead of adding a new one || means that when you click on the link, it will navigate to the new page without adding a new entry to the browser's history. This can be useful in situations where you want to prevent users from going back to the previous page using the back button, such as after a form submission or when navigating through a multi-step process.
       </h2>
       <h2>
         <Link href={`/products/${productId}`}>Product ${productId}</Link>
