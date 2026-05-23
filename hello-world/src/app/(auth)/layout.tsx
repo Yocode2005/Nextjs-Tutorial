79047b890c0192436a831a3365e28c98479ea3c8
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-23 22:35:38 +0530

    learn active link concept

--- hello-world/src/app/(auth)/layout.tsx
+++ hello-world/src/app/(auth)/layout.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const navLinks = [
   { name : "Register", href: "/register" },
@@ -7,11 +10,13 @@ const navLinks = [
 ];
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname();
   return (
     <div>
         {navLinks.map((link) => {
+            const isActive = pathname === link.href || (pathname.startsWith(link.href) && link.href !== "/");
             return(
-                <Link href={link.href} key={link.name}>
+                <Link className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"} href={link.href} key={link.name}>
                     {link.name}
                 </Link>
             );
