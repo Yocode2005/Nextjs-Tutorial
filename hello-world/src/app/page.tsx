dfd480af3ab04cf5f46a05f30c0c5dc10f93e066
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-23 21:58:54 +0530

    learn the way of navigation in nextjs

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return(
+    <>
+    <h1>Welcome  to the Home Page</h1>
+    <Link href="/blog">Go to Blog</Link>
+    </>
+  ) 
 }
