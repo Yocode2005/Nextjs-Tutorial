74bc0d831fc891adca97894baf159578b2a4c4fb
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-26 22:28:32 +0530

    add new page

---
+++ hello-world/src/app/articles/[articleId]/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+export default function NewsArticle(){
+    return (
+        <div>
+            <h1>News article id</h1>
+            <p>Reading in language</p>
+            <div>
+                <Link href="/articles/id?lang=en">Read in English</Link>
+                <Link href="/articles/id?lang=es">Read in Spanish</Link>
+                <Link href="/articles/id?lang=fr">Read in French</Link>
+            </div>
+        </div>
+    )
+}
