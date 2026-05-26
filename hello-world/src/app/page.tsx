39ac2c516c6e9fc258667bf8729c6783dcef543c
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-26 22:30:18 +0530

    add br tag for convenies

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -7,7 +7,11 @@ export default function Home() {
     <Link href="/blog">Go to Blog</Link>
     <br />
     <Link href="/products">Go to Product</Link>
+    <br />
     <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
+    <br />
+    <Link href="/articles/breaking-news-123?lang=es">Read in Spanish</Link>
+    <br />
     <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
     </>
   ) 
