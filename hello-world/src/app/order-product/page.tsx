1872d86a328baa45aab22bec632f67b4d12c702e
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-26 22:55:00 +0530

    learning the concept of useRouter

--- hello-world/src/app/order-product/page.tsx
+++ hello-world/src/app/order-product/page.tsx
@@ -1,3 +1,17 @@
+import {useRouter} from 'next/navigation';
+
+
 export default function OrderProduct(){
-    return <h1>Order Product</h1>
+    const router = useRouter();
+    const handleClick = () => {
+        console.log("Order placed successfully!");
+        router.push('/'); //  Redirect to home page after placing the order
+    }
+    return(
+        <>
+        <h1>Order Product</h1>
+        <button onClick={handleClick}>Place Order</button>
+        </>
+    )
+     
 }
