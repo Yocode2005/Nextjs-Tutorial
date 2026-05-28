97bb1ffcc216bfcce2c328bd732fe6f3a2d13a51
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-05-28 19:00:39 +0530

    learn advance concepts of error handling

--- hello-world/src/app/products/[productid]/layout.tsx
+++ hello-world/src/app/products/[productid]/layout.tsx
@@ -1,8 +1,16 @@
+function getRandomInt(count : number){
+    return Math.floor(Math.random()  * count);
+}
+
 export default function ProductDetailsLayout({
     children,
 }: {
     children: React.ReactNode // use React.ReactNode to allow any valid React child (string, element, fragment, etc.)
 }){
+    const random = getRandomInt(2);
+    if(random == 1){
+        throw new Error("Error loading product details"); // Simulate a random error to demonstrate error handling in Next.js. This will cause the page to display an error message when the random number is 1.
+    }
     return (
         <>
         {children}
