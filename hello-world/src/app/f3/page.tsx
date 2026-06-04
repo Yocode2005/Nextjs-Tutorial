7589d4558bff9620f3eadeb40eef5771a26af28c
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-04 12:45:06 +0530

    remove unwanted links

--- hello-world/src/app/f3/page.tsx
+++ hello-world/src/app/f3/page.tsx
@@ -3,11 +3,6 @@ export default function F3(){
     return (
         <>
         <h1> F3 page</h1>
-        <div>
-        <Link href="/f1/f2">Go  back to F2</Link>
-        <br />
-        <Link href="/f1">Go  back to F1</Link>
-        </div>
         </>
     )
 }
