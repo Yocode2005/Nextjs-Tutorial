cbe6133da4bac1471d2e0476453dad651e9b7ef5
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-04 12:24:15 +0530

    add more link

--- hello-world/src/app/f1/page.tsx
+++ hello-world/src/app/f1/page.tsx
@@ -5,8 +5,9 @@ export default function F1 (){
         <>
         <h1> F1 Page </h1>
         <div>
-            <Link href="/f1/f2">Go to F2</Link>
-            <Link href="/f1/f3">Go to F3</Link>
+            <Link href={"/f1/f2"}>Go to F2</Link>
+            <br />
+            <Link href={"/f3"}>Go to F3</Link>
         </div>
         </>
     );
--- hello-world/src/app/f3/page.tsx
+++ hello-world/src/app/f3/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 export default function F3(){
     return (
         <>
         <h1> F3 page</h1>
+        <div>
+        <Link href="/f1/f2">Go  back to F2</Link>
+        <br />
+        <Link href="/f1">Go  back to F1</Link>
+        </div>
         </>
     )
 }
