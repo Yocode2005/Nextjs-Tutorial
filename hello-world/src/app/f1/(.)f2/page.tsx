d9f7b3119aeec01ee052f3dcabf0d0dc2209b6f9
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-04 12:16:12 +0530

    add important conventions

---
+++ hello-world/src/app/f1/(.)f2/page.tsx
@@ -0,0 +1,7 @@
+export default function InterceptedF2(){
+    return <h1>(.) Intercepted F2 page</h1>
+}
+
+// intercepting routes conventions
+// (.) to match segments on the same level
+// (..) to match segments one level above
