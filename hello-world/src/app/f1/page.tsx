26e86f8d31176bd9230b20cc0856b3ccca9c4721
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-04 12:45:55 +0530

    explore intercepting routes in more details

--- hello-world/src/app/f1/page.tsx
+++ hello-world/src/app/f1/page.tsx
@@ -8,6 +8,7 @@ export default function F1 (){
             <Link href={"/f1/f2"}>Go to F2</Link>
             <br />
             <Link href={"/f3"}>Go to F3</Link>
+            <br />
         </div>
         </>
     );
