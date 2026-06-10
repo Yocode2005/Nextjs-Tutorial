44da60ae2f09beed63c070f3ea4dbe286785b15d
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-10 12:06:04 +0530

    cookies in route handling

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import {type NextRequest} from "next/server";
-import {headers} from "next/headers"; // This is a server component, so we can use the headers() function to access the request headers directly.
+import {headers,cookies} from "next/headers"; // This is a server component, so we can use the headers() function to access the request headers directly.and cookies() function to access the cookies directly without needing to use the request object.
 
 export async function GET(request: NextRequest){
     // const requestHeaders = new Headers(request.headers);
@@ -10,6 +10,10 @@ export async function GET(request: NextRequest){
 
     const theme = request.cookies.get("theme"); // use the cookies property of the request object to access cookies
     console.log(theme);
+
+    const cookieStore = await cookies();
+    cookieStore.set("resultsPerPage","20") // use the cookies() function to get a cookie store and set a cookie in the response
+    console.log(cookieStore.get("resultsPerPage"));
 
     return new Response("<h1>Profile API data</h1>",{
         headers: {
