f60b5817da5446780085dd893e02aedc57f6aec9
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-10 11:22:14 +0530

    handling query parameters

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,11 +1,19 @@
-
 import { comments } from "./data";
+import { NextRequest } from "next/server";
 
-export async function GET() {
-  return Response.json(comments);
+
+export async function GET(request: NextRequest) {
+ const searchParams = request.nextUrl.searchParams;
+ const query = searchParams.get("query");
+ const filteredComments = query
+   ? comments.filter((comment) =>
+       comment.text.includes(query)
+     )
+   : comments;
+ return  Response.json(filteredComments); 
 }
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const comment = await request.json();
   const newComment = {
     id: comments.length + 1,
