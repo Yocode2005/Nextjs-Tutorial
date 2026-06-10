3b1a6f710b99c42cfcee013bb1d1fa327262f640
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-10 11:05:42 +0530

    add important comments

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -1,6 +1,10 @@
 import {comments} from "../data";
 
 export async function GET(_request: Request, {params}: {params: Promise<{id: string}>}) {
+    // todolist for get comment by id
+    // 1. Get the id from params
+    // 2. Find the comment with the given id
+    // 3. Return the comment as response
     const {id} = await params;
     const comment = comments.find((comment) => comment.id === parseInt(id));
     return Response.json(comment);
