858d374be966c90ecdff447bc414b96eaf6108a8
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-10 10:59:07 +0530

    handling patch request

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -5,3 +5,17 @@ export async function GET(_request: Request, {params}: {params: Promise<{id: str
     const comment = comments.find((comment) => comment.id === parseInt(id));
     return Response.json(comment);
 }
+
+export async function PATCH(
+    request : Request,
+    {params}: {params: Promise<{id: string}>}
+) {
+    const {id} = await params;
+    const body = await request.json();
+    const {text} = body;
+
+    const index = comments.findIndex((comment) => comment.id === parseInt(id)); // Find the index of the comment to update
+    comments[index].text = text;// Update the comment's text
+
+    return Response.json(comments[index]);
+}
