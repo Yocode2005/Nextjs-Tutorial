35c9dec2f9e92407ba3c22a7add0302ef9178fa2
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-10 11:43:23 +0530

    add comment to understand

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import {type NextRequest} from "next/server";
-import {headers} from "next/headers";
+import {headers} from "next/headers"; // This is a server component, so we can use the headers() function to access the request headers directly.
 
 export async function GET(request: NextRequest){
     // const requestHeaders = new Headers(request.headers);
