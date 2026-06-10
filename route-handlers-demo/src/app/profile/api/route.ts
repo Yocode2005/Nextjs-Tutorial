527327436afeb8c77256e5bb3917fadbeeb0db61
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-10 11:42:41 +0530

    headers in route hsndling

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,7 +1,11 @@
 import {type NextRequest} from "next/server";
+import {headers} from "next/headers";
 
 export async function GET(request: NextRequest){
-    const requestHeaders = new Headers(request.headers);
-    console.log(requestHeaders.get("Authorization"));
+    // const requestHeaders = new Headers(request.headers);
+    // console.log(requestHeaders.get("Authorization"));
+
+    const headerList = await headers();
+    console.log(headerList.get("Authorization"));
     return new Response("Profile API data");
 }
