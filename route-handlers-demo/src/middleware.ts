168cbf8090ac22f61575217982767b1da2377708
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-12 22:08:31 +0530

    add commet

--- route-handlers-demo/src/middleware.ts
+++ route-handlers-demo/src/middleware.ts
@@ -4,6 +4,6 @@ export function middleware(request : NextRequest) {
     return NextResponse.redirect(new URL("/categories",request.url));
 }
 
-export const config = {
+export const config = { // This matcher tells Next.js to apply this middleware only to requests that match the specified path. In this case, the middleware will only be applied to requests made to the "/profile" path. This allows us to control which routes are affected by the middleware and ensures that it only runs when necessary.
     matcher: "/profile",
 }
