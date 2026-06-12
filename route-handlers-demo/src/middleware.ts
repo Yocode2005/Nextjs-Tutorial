b064def968c54faf6c2d3fce6621e0dc747f2649
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-12 22:11:49 +0530

    explore second approch

--- route-handlers-demo/src/middleware.ts
+++ route-handlers-demo/src/middleware.ts
@@ -1,9 +1,15 @@
 import {NextRequest, NextResponse} from "next/server";
 
-export function middleware(request : NextRequest) {
-    return NextResponse.redirect(new URL("/categories",request.url));
-}
+// export function middleware(request : NextRequest) {
+//     return NextResponse.redirect(new URL("/categories",request.url));
+// }
 
-export const config = { // This matcher tells Next.js to apply this middleware only to requests that match the specified path. In this case, the middleware will only be applied to requests made to the "/profile" path. This allows us to control which routes are affected by the middleware and ensures that it only runs when necessary.
-    matcher: "/profile",
+// export const config = { // This matcher tells Next.js to apply this middleware only to requests that match the specified path. In this case, the middleware will only be applied to requests made to the "/profile" path. This allows us to control which routes are affected by the middleware and ensures that it only runs when necessary.
+//     matcher: "/profile",
+// }
+
+export function middleware(request : NextRequest) {
+    if(request.nextUrl.pathname === "/profile"){
+        return NextResponse.redirect(new URL("/hello",request.nextUrl));
+    }
 }
