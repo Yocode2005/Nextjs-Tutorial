80c2b2a9d1d27dd818bd4a6f950f9a17784b6db7
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-06-12 22:05:39 +0530

    add middleware

--- route-handlers-demo/src/middleware.ts
+++ route-handlers-demo/src/middleware.ts
@@ -3,3 +3,7 @@ import {NextRequest, NextResponse} from "next/server";
 export function middleware(request : NextRequest) {
     return NextResponse.redirect(new URL("/",request.url));
 }
+
+export const config = {
+    matcher: "/profile",
+}
