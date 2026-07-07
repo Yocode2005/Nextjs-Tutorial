4f7b60f4ef46a4686e27adea8fa604884f1fc4a7
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-07-07 17:56:25 +0530

    add new route

--- rendering-demo/app/client-route/page.tsx
+++ rendering-demo/app/client-route/page.tsx
@@ -1,9 +1,31 @@
 "use client";
-import {serverSideFunction} from "../utils/server-utils";
+
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 export default function ClientRoutePage() {
-  const result = serverSideFunction();
+   const settings = {
+    dots: true,
+  };
   return (
-      <h1>Client route page {result}</h1>
+    <div className="image-slider-container">
+      <Slider {...settings}>
+        <div>
+          <img src="http://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="http://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="http://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="http://picsum.photos/400/200" />
+        </div>
+      </Slider>
+    </div>
   );
 }
